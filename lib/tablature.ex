9221c3439e634c66c8f906807ecd9ffc5a023c9b
Dylan Isaac Mancilla <isaac_mancilla@MacBook-Air-de-Dylan.local>
2026-05-06 12:44:31 -0600

    Parte 1

--- lib/tablature.ex
+++ lib/tablature.ex
@@ -1,8 +1,21 @@
 defmodule Tablature do
   def parse(tab) do
-    Regex.scan(~r/\d+/, tab)
-    |> List.flatten
-    |> Enum.map(fn n -> "B" <> n end)
+  tab
+    |> String.split("\n", trim: true)
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, row_idx} ->
+      [label, content | _] = String.split(line, "|")
+
+      content
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.reject(fn {char, _} -> char =~ ~r/\D/ end)
+      |> Enum.map(fn {digit, col_idx} ->
+        {col_idx, row_idx, "#{label}#{digit}"}
+      end)
+    end)
+    |> Enum.sort()
+    |> Enum.map(fn {_, _, note} -> note end)
     |> Enum.join(" ")
   end
 end
